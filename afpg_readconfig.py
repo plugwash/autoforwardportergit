d5ef094ae3bfb910187da5d7b8fa2695fb5486f6
Peter Michael Green <plugwash@debian.org>
2017-08-05 12:22:12 +0100

    Add inital test for deraspbianised dscdirtogitdriver. Fix up issues revealed by it.

--- afpg_readconfig.py
+++ afpg_readconfig.py
@@ -12,6 +12,8 @@ if not os.path.isdir(configdir):
 	sys.stderr.write('autoforwardportergit config directory '+configdir+" does not exist or is not a directory\n")
 	sys.exit(1)
 
+sys.stderr.write('using autoforwardportergit config directory '+configdir+"\n")
+
 #print(repr(configdir))
 configfile = os.path.join(configdir,'afpg.ini')
 config = configparser.ConfigParser()
@@ -21,12 +23,18 @@ config['main']['dodgitpush'] = 'no';
 config['main']['AFPGCONFIG'] = configdir;
 config.read(configfile)
 
+def readconfigentry(section,item):
+	value = config[section][item];
+	if ((item == 'workingrepo') | (item == 'gitdir') | (item == 'tmp')):
+		value = os.path.join(configdir,value)
+	return value
+
 if __name__ == '__main__': #if we are being run directly rather than imported
 	#print(repr(config['main']))
 	#print("AFPGCONFIG='"+configdir+"'");
 	#for (name,value) in config['main'].items():
 	#	print(name+"='"+value+"'");
 	for arg in sys.argv[1:]:
-		(section,name) = arg.split(':',1)
-		print(name+"='"+config[section][name]+"'")
+		(section,item) = arg.split(':',1)
+		print(item+"='"+readconfigentry(section,item)+"'")
 
